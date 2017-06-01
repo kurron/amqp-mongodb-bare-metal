9a280d4d39659c813634ffdbb9aad76b17f3a316
Ron Kurr <kurr@kurron.org>
2017-06-01 14:04:56 -0400

    Can now specify the MongoDB connection string.

--- run-container.sh
+++ run-container.sh
@@ -8,6 +8,7 @@ MOD_COUNT=${5:-100}
 EXPECTED_COUNT=${6:-2500}
 PREFETCH_COUNT=${7:-1}
 CONCURRENCY=${8:-1}
+MONGODB_URL=${9:-mongodb://localhost:27017/bare-metal?safe=true&w=majority&readPreference=nearest&appName=amqp-mongodb-bare-metal}
 
 CMD="docker run \
             --cpus 1 \
@@ -21,6 +22,7 @@ CMD="docker run \
             --env spring_rabbitmq_listener_concurrency=${CONCURRENCY} \
             --env spring_rabbitmq_listener_max-concurrency=${CONCURRENCY} \
             --env spring_rabbitmq_cache_channel_size=${CONCURRENCY} \
+            --env spring_data_mongodb_uri=${MONGODB_URL} \
             --env consumer_modvalue=${MOD_COUNT} \
             --env consumer_expectedMessageCount=${EXPECTED_COUNT} \
             --interactive  \
